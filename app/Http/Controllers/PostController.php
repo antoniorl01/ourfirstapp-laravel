a4bd28a42a2977e802c206e41099e829fbcb12e8
Antonio Rubia <antonio.rl.1011@gmail.com>
2024-09-01 15:40:49 +0200

    added post creation page

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('create-post');
+    }
+
+    public function storeNewPost(Request $request) 
+    {
+        $body = $request->validate([
+            "title" => "required",
+            "body" => "required"
+        ]);
+
+        $body["title"] = strip_tags($body["title"]);
+        $body["body"] = strip_tags($body["body"]);
+        $body["user_id"] = auth()->id(); // get the user id from the session
+
+        Post::create($body);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
+// User related routes
 Route::get('/', [UserController::class, "homepage"]);
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
 
+// Blog post related routes
+Route::get('/create-post', [PostController::class], "showCreateForm");
+Route::post('/create-post', [PostController::class], "storeNewPost");
 
 
