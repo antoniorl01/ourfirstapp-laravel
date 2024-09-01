077c6c338588237538db77b68364368c0a076e26
Antonio Rubia <antonio.rl.1011@gmail.com>
2024-09-01 12:27:07 +0200

    remove example controller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [UserController::class, "homepage"]);
-Route::get('/about', [ExampleController::class, "aboutpage"]);
-
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
